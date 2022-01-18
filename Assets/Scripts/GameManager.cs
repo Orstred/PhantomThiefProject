ff6d39c1fd3346e577d0a4d91ac483c4ff217984
Guits <guilhermessecoortiz@gmail.com>
2022-01-18 13:36:09 +0000

    OST Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -57,9 +57,9 @@ public class GameManager : MonoBehaviour
 
     [HorizontalLine(2, EColor.Gray)]
     [Header("AUDIO OPTIONS")]
-    [Range(0,100)]
+    [Range(0,1)]
     public float MainVolume = 1f;
-    [Range(0, 100)]
+    [Range(0, 1)]
     public float SFXVolume = 1f;
     public SoundClip[] OST;
 
@@ -92,15 +92,46 @@ public class GameManager : MonoBehaviour
 
     public void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            PlayOST(0);
+        }
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            PlayOST("Raid");
+            PlayOST(1);
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            PlayOST("Techno");
+            PlayOST(2);
         }
-
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            PlayOST(3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            PlayOST(4);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            PlayOST(5);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            PlayOST(6);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            PlayOST(7);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            PlayOST(8);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            PlayOST(9);
+        }
     }
 
 
@@ -121,7 +152,11 @@ public class GameManager : MonoBehaviour
         s.source.Play();
     }
 
-
+    public void PlayOST(int Track)
+    {
+        if(OST[Track] != null)
+        OST[Track].source.Play();
+    }
 }
 
 
